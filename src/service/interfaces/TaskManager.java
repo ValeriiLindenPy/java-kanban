b80bafec693318f8a151d747a87a0f8266641db7
ValeriiLindenPy <115076024+ValeriiLindenPy@users.noreply.github.com>
2024-08-13 13:53:19 +0200

    доработана функциональность по тз спринта 6-3

--- src/service/interfaces/TaskManager.java
+++ src/service/interfaces/TaskManager.java
@@ -7,24 +7,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface TaskManager {
+
     ArrayList<Task> getTasks();
+
     ArrayList<Subtask> getSubTasks();
+
     ArrayList<Epic> getEpics();
+
     void deleteTasks();
+
     void deleteSubTasks();
+
     void deleteEpics();
+
     Task getTaskByID(int id);
+
     Subtask getSubTaskByID(int id);
+
     Epic getEpicByID(int id);
+
     int createTask(Task task);
+
     int createSubTask(Subtask task);
+
     int createEpicTask(Epic task);
+
     void updateTask(Task task);
+
     void updateSubTask(Subtask task);
+
     void updateEpicTask(Epic task);
+
     void deleteTaskByID(int id);
+
     void deleteSubTaskByID(int id);
+
     void deleteEpicByID(int id);
+
     ArrayList<Subtask> getEpicSubTasks(int epicID);
+
     List<Task> getHistory();
 }

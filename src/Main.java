5e608ce34336e0b758300378238e7b85c0150df1
ValeriiLindenPy <115076024+ValeriiLindenPy@users.noreply.github.com>
2024-09-09 15:00:22 +0200

    доработана функциональность по тз спринта 8-1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-import model.Epic;
-import model.Status;
-import model.Subtask;
-import model.Task;
-import service.InMemoryTaskManager;
-import service.Managers;
-import service.interfaces.TaskManager;
-
-import java.time.Duration;
-import java.util.Comparator;
-import java.util.Objects;
-import java.util.TreeSet;
-
 public class Main {
     /**
      * Все тесты находятся в папке test
      */
 
     public static void main(String[] args) {
-
     }
 }
